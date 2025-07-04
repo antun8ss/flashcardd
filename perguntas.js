75360dd9f051348ce2d23cc058f48b9d16d8437b
Gabriel Rocha <gabriel.rocha.antunes@escola.pr.gov.br>
2025-07-04 09:40:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimento',
+    'Qual alimento não estraga?',
+    'O mel não estraga com o tempo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior deserto do mundo?',
+    'O maior deserto do mundo é a Antártida.'
+)
+
+criaCartao(
+    'Conhecimento',
+    'Um raio de sol leva cerca de quanto tempo para atingir a terra?',
+    'O raio de sol leva cerca de 8 minutos para atingir a terra.'
+)
+
+criaCartao(
+    'Conhecimento',
+    'O coração humano bate cerca de quantas vezes por dia?',
+    '100.000 por dia.'
+)
